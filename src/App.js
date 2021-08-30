94f783592a47d29dc0757f7b4119966558d58080
Yonathan <nathanffel@gmail.com>
2021-08-30 16:57:16 +1000

    Updated the style

--- src/App.js
+++ src/App.js
@@ -19,13 +19,13 @@ function App() {
     <div className="App">
       <div className="container">
         <div className="button-wrapper">
-          <input type="text" placeholder="Tracking Number" className="input-tracking" onChange={handleInputChange} />
-          <button type="button" className="search-button" onClick={getData}>Search</button>
+          <input type="text" placeholder="Введите код отслеживания" className="input-tracking" onChange={handleInputChange} />
+          <button type="button" className="search-button" onClick={getData}>Найти</button>
         </div>
         {data.data.length > 0 ? (
           <div className="result-container">
-            <h1 className="title">Hi there,</h1>
-            <p className="sub-title">Your tracking number {data.tracking_number}</p>
+            <h1 className="title">Добрый день,</h1>
+            <p className="sub-title">Ваш код отслеживания {data.tracking_number}</p>
             <div className="results">
               {data.data.map((res, index) => (
                 <div className={`result ${index % 2 === 1 ? 'white' : ''}`} key={index}>
