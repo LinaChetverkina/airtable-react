65fe576e13c528752ce29ba652d30f430bc82fee
Yonathan <nathanffel@gmail.com>
2021-08-30 16:45:01 +1000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  const [tracking, setTracking] = useState('');
+  const [data, setData] = useState({ data: [] });
+
+  const getData = async () => {
+    const result = await axios.post(`${process.env.REACT_APP_API_URL}/track`, { trackingNumber: tracking });
+    setData(result.data);
+  };
+
+  const handleInputChange = e => {
+    setTracking(e.target.value);
+  };
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="button-wrapper">
+          <input type="text" placeholder="Tracking Number" className="input-tracking" onChange={handleInputChange} />
+          <button type="button" className="search-button" onClick={getData}>Search</button>
+        </div>
+        {data.data.length > 0 ? (
+          <div className="result-container">
+            <h1 className="title">Hi there,</h1>
+            <p className="sub-title">Your tracking number {data.tracking_number}</p>
+            <div className="results">
+              {data.data.map((res, index) => (
+                <div className={`result ${index % 2 === 1 ? 'white' : ''}`} key={index}>
+                  <p className="date">{res.date}</p>
+                  <p className="status">{res.stage}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );
+}
+
+export default App;
