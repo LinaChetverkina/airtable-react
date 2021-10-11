5d2b8af50119840d63283253bc86f681f233ff8a
Yonathan <nathanffel@gmail.com>
2021-10-11 19:44:30 +1100

    Added Translation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,21 @@ import { useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
+const text = {
+  ru: {
+    placeholder: 'Введите код отслеживания',
+    button: 'Найти',
+    title: 'Добрый день,',
+    subtitle: 'Ваш код отслеживания'
+  },
+  en: {
+    placeholder: 'Type your tracking number here',
+    button: 'Search',
+    title: "G'day!",
+    subtitle: 'Your tracking number is'
+  }
+};
+
 function App() {
   const [tracking, setTracking] = useState('');
   const [data, setData] = useState({ data: [] });
@@ -15,17 +30,20 @@ function App() {
     setTracking(e.target.value);
   };
 
+  const params = new URLSearchParams(window.location.search);
+  const lang = params.get("lang") === "en" ? "en" : "ru";
+
   return (
     <div className="App">
       <div className="container">
         <div className="button-wrapper">
-          <input type="text" placeholder="Введите код отслеживания" className="input-tracking" onChange={handleInputChange} />
-          <button type="button" className="search-button" onClick={getData}>Найти</button>
+          <input type="text" placeholder={text[lang].placeholder} className="input-tracking" onChange={handleInputChange} />
+          <button type="button" className="search-button" onClick={getData}>{text[lang].button}</button>
         </div>
         {data.data.length > 0 ? (
           <div className="result-container">
-            <h1 className="title">Добрый день,</h1>
-            <p className="sub-title">Ваш код отслеживания {data.tracking_number}</p>
+            <h1 className="title">{text[lang].title}</h1>
+            <p className="sub-title">{text[lang].subtitle} {data.tracking_number}</p>
             <div className="results">
               {data.data.map((res, index) => (
                 <div className={`result ${index % 2 === 1 ? 'white' : ''}`} key={index}>
